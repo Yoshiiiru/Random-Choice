c2bb4cb95a076a0902c386defac45425620f1bc3
Yoshiiiru <113729989+Yoshiiiru@users.noreply.github.com>
2023-02-22 13:55:45 -0500

    Update RandomChoice.py

--- RandomChoice.py
+++ RandomChoice.py
@@ -1,1 +1,18 @@
-print("Hello World")
+import random
+
+choice = input("Enter Chore, Activity, or Meal: ")
+
+if choice == 'Chore':
+
+    print("Your chore is")
+
+elif choice == 'Activity':
+
+    print("Your activity is")
+
+elif choice == 'Meal':
+
+    print("Your meal is")
+
+else:
+    print("Invalid Choice")
