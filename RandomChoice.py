7e18c13a1501c4b909c1c4abf67488b2feca6be9
Yoshiiiru <113729989+Yoshiiiru@users.noreply.github.com>
2023-02-22 14:02:56 -0500

    Update RandomChoice.py

--- RandomChoice.py
+++ RandomChoice.py
@@ -2,17 +2,25 @@ import random
 
 choice = input("Enter Chore, Activity, or Meal: ")
 
+Meals = ['Sausage and Rice', 'Fish and Rice', 'Chicken and Rice']
+Chores = ['Fishtank', 'Vaccum', 'Kitchen']
+Activities = ['Puzzle', 'Movie', 'Games']
+
+Meal_Choice = random.choice(Meals)
+Chores_Choice = random.choice(Chores)
+Activities_Choice = random.choice(Activities)
+
 if choice == 'Chore':
 
-    print("Your chore is")
+    print("Your chore is " + Chores_Choice)
 
 elif choice == 'Activity':
 
-    print("Your activity is")
+    print("Your activity is " + Activities_Choice)
 
 elif choice == 'Meal':
 
-    print("Your meal is")
+    print("Your meal is " + Meal_Choice)
 
 else:
     print("Invalid Choice")
